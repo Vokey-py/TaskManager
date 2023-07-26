533c863628703a8dd507f9094ec0b89d1b826b49
Vokey-py <139166417+Vokey-py@users.noreply.github.com>
2023-07-26 15:32:58 +0300

    Добавил permissions

--- main/api/views.py
+++ main/api/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .permissions import IsOwnerTask
+
 from taskmanager.models import Task
 from .serializers import TaskSerializer
 
-
 class TaskManagerViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsOwnerTask, IsAuthenticated]
+    def get_queryset(self):
+        # Вернуть только те записи, которые принадлежат текущему пользователю.
+        queryset = Task.objects.filter(user=self.request.user)
+        return queryset
