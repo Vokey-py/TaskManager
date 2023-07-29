3dd5238c502af0953ce18e605dfdf15b00bc8dbb
Vokey-py <139166417+Vokey-py@users.noreply.github.com>
2023-07-29 15:31:32 +0300

    Изучаю React Js

---
+++ main/frontend/react/taskmanager/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://webcomplex.com.ua/wp-content/uploads/2014/11/slider.png'/>
+            <div>
+                ava + Description
+            </div>
+            <div>
+                my post
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    pos21
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile
