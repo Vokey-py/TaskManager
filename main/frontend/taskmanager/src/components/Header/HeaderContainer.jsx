7c6efb2ee4a44c766736fc22d533393a7045bc1b
Vokey-py <139166417+Vokey-py@users.noreply.github.com>
2023-08-11 15:17:47 +0300

    Подключил подписку Follow через post and delete запрос

---
+++ main/frontend/taskmanager/src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Header";
+import axios from "axios";
+import {setAuthUserData} from "../../redux/reducerAuth";
+import {connect} from "react-redux";
+
+class HeaderApiContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true})
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let {id, login, email} = response.data.data
+                    this.props.setAuthUserData(id, email, login)
+                }
+
+            });
+    }
+
+    render() {
+        return <Header {...this.props}/>
+    }
+}
+
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+})
+export default connect(mapStateToProps, {setAuthUserData})(HeaderApiContainer);
